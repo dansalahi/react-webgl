2c686c06db7b870cf5c078f7c7cf9a30e575f879
dan salahi <dan.salahii@gmail.com>
2022-08-07 22:08:51 +0300

    added the use unity context

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Unity, useUnityContext } from 'react-unity-webgl'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "unitybuild/2020.1/myunityapp.loader.js",
+    dataUrl: "unitybuild/2020.1/myunityapp.data",
+    frameworkUrl: "unitybuild/2020.1/myunityapp.framework.js",
+    codeUrl: "unitybuild/2020.1/myunityapp.wasm",
+    streamingAssetsUrl: "unitybuild/2020.1/streamingassets",
+    // Additional configuration options.
+    webglContextAttributes: {
+      preserveDrawingBuffer: true,
+    },
+  })
+  return <Unity unityProvider={unityProvider} style={{ width:980, height:700 }}></Unity>
 }
 
-export default App;
+
+export default App
